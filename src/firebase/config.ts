d62abaa404aaf830c37cccc790249ef42b3acffc
Adrian Bengolea <abengolea1@gmail.com>
2026-01-29 22:37:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "qualified-justice",
+  "appId": "1:539143735533:web:a7de3b8b5904e7e2d864e4",
+  "apiKey": "AIzaSyC0Xr50Pj6rbhjOOxdr77ielzCbhD-0lWs",
+  "authDomain": "qualified-justice.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "539143735533"
+};
