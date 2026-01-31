aeef25bf3e548841cb0783e98a9204db7b3a63cf
Adrian Bengolea <abengolea1@gmail.com>
2026-01-31 16:49:03 +0000

    solo 6 juzgados se cargaron!!! tenia mas de 3000

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -83,5 +83,185 @@ export const mockCourthouses: Omit<Courthouse, 'id'>[] = [
     instancia: "Primera Instancia",
     direccion: "Talcahuano 550",
     telefono: "011-4370-4500"
+  },
+  {
+    nombre: "Juzgado de Garantías N° 3",
+    dependencia: "Buenos Aires",
+    ciudad: "Lomas de Zamora",
+    fuero: "Penal",
+    instancia: "Primera Instancia",
+    direccion: "Laprida 650",
+    telefono: "011-4244-1234"
+  },
+  {
+    nombre: "Tribunal Oral en lo Criminal N° 15",
+    dependencia: "Ciudad Autónoma de Buenos Aires",
+    ciudad: "CABA",
+    fuero: "Penal",
+    instancia: "Tribunal Oral",
+    direccion: "Paraguay 1536",
+    telefono: "011-4813-5566"
+  },
+  {
+    nombre: "Juzgado de Primera Instancia en lo Civil y Comercial N° 7",
+    dependencia: "Santa Fe",
+    ciudad: "Rosario",
+    fuero: "Civil y Comercial",
+    instancia: "Primera Instancia",
+    direccion: "Balcarce 1651",
+    telefono: "0341-472-1700"
+  },
+  {
+    nombre: "Cámara de Apelaciones en lo Civil y Comercial - Sala II",
+    dependencia: "Córdoba",
+    ciudad: "Córdoba",
+    fuero: "Civil y Comercial",
+    instancia: "Cámara de Apelación",
+    direccion: "Duarte Quirós 550",
+    telefono: "0351-448-1000"
+  },
+  {
+    nombre: "Juzgado de Paz de San Miguel del Monte",
+    dependencia: "Buenos Aires",
+    ciudad: "San Miguel del Monte",
+    fuero: "Paz",
+    instancia: "Juzgado de Paz",
+    direccion: "Rivadavia 450",
+    telefono: "02271-42-0545"
+  },
+  {
+    nombre: "Juzgado Federal de Salta N° 2",
+    dependencia: "Justicia Federal",
+    ciudad: "Salta",
+    fuero: "Federal",
+    instancia: "Juzgado Federal de Primera Instancia",
+    direccion: "España 394",
+    telefono: "0387-431-7777"
+  },
+  {
+    nombre: "Tribunal del Trabajo N° 1 de Avellaneda",
+    dependencia: "Buenos Aires",
+    ciudad: "Avellaneda",
+    fuero: "Laboral",
+    instancia: "Tribunal de Instancia Única",
+    direccion: "Av. Mitre 541",
+    telefono: "011-4201-9876"
+  },
+  {
+    nombre: "Juzgado Nacional de Primera Instancia del Trabajo N° 80",
+    dependencia: "Justicia Nacional",
+    ciudad: "CABA",
+    fuero: "Laboral",
+    instancia: "Primera Instancia",
+    direccion: "Lavalle 1268",
+    telefono: "011-4379-1500"
+  },
+  {
+    nombre: "Juzgado de Menores N° 1",
+    dependencia: "Mendoza",
+    ciudad: "Mendoza",
+    fuero: "Menores",
+    instancia: "Primera Instancia",
+    direccion: "San Martín 322",
+    telefono: "0261-449-3300"
+  },
+  {
+    nombre: "Cámara Federal de Apelaciones de La Plata - Sala I",
+    dependencia: "Justicia Federal",
+    ciudad: "La Plata",
+    fuero: "Federal",
+    instancia: "Cámara de Apelación",
+    direccion: "Calle 8 N° 925",
+    telefono: "0221-425-9001"
+  },
+  {
+    nombre: "Juzgado de Primera Instancia en lo Penal Económico N° 2",
+    dependencia: "Justicia Nacional",
+    ciudad: "CABA",
+    fuero: "Penal Económico",
+    instancia: "Primera Instancia",
+    direccion: "Av. de los Inmigrantes 1950",
+    telefono: "011-5167-5000"
+  },
+    {
+    nombre: "Juzgado de Familia N° 1 de Morón",
+    dependencia: "Buenos Aires",
+    ciudad: "Morón",
+    fuero: "Familia",
+    instancia: "Primera Instancia",
+    direccion: "Almirante Brown 955",
+    telefono: "011-4629-1051"
+  },
+  {
+    nombre: "Cámara Nacional de Apelaciones en lo Comercial - Sala A",
+    dependencia: "Justicia Nacional",
+    ciudad: "CABA",
+    fuero: "Comercial",
+    instancia: "Cámara de Apelación",
+    direccion: "Diagonal Roque Sáenz Peña 1211",
+    telefono: "011-4379-1100"
+  },
+  {
+    nombre: "Juzgado en lo Civil y Comercial N° 14 de Mar del Plata",
+    dependencia: "Buenos Aires",
+    ciudad: "Mar del Plata",
+    fuero: "Civil y Comercial",
+    instancia: "Primera Instancia",
+    direccion: "Almirante Brown 2046",
+    telefono: "0223-495-1200"
+  },
+  {
+    nombre: "Juzgado Federal de San Juan",
+    dependencia: "Justicia Federal",
+    ciudad: "San Juan",
+    fuero: "Federal",
+    instancia: "Juzgado Federal de Primera Instancia",
+    direccion: "Entre Ríos 281 Sur",
+    telefono: "0264-422-5588"
+  },
+  {
+    nombre: "Tribunal Fiscal de la Nación",
+    dependencia: "Justicia Nacional",
+    ciudad: "CABA",
+    fuero: "Fiscal",
+    instancia: "Tribunal Fiscal",
+    direccion: "Alsina 970",
+    telefono: "011-4343-4151"
+  },
+  {
+    nombre: "Juzgado de Ejecución Penal N° 1 de San Isidro",
+    dependencia: "Buenos Aires",
+    ciudad: "San Isidro",
+    fuero: "Ejecución Penal",
+    instancia: "Primera Instancia",
+    direccion: "Ituzaingó 340",
+    telefono: "011-4747-1122"
+  },
+  {
+    nombre: "Cámara Federal de Apelaciones de Tucumán",
+    dependencia: "Justicia Federal",
+    ciudad: "San Miguel de Tucumán",
+    fuero: "Federal",
+    instancia: "Cámara de Apelación",
+    direccion: "Pasaje Vélez Sarsfield 450",
+    telefono: "0381-431-1044"
+  },
+  {
+    nombre: "Juzgado de Primera Instancia de Distrito en lo Laboral N° 5 de Santa Fe",
+    dependencia: "Santa Fe",
+    ciudad: "Santa Fe",
+    fuero: "Laboral",
+    instancia: "Primera Instancia",
+    direccion: "3 de Febrero 2761",
+    telefono: "0342-457-2700"
+  },
+  {
+    nombre: "Cámara de Apelación y Garantías en lo Penal de Bahía Blanca",
+    dependencia: "Buenos Aires",
+    ciudad: "Bahía Blanca",
+    fuero: "Penal",
+    instancia: "Cámara de Apelación",
+    direccion: "Estomba 34",
+    telefono: "0291-455-5011"
   }
 ];
